5e9b1fb8a9d97f72bc1ffb42363e15b63d727fd4
KevinYSM <22753615@student.uwa.edu.au>
2022-12-06 15:31:55 +0800

    added set -

--- scripts/data_manipulation/standard_rnaseq_workflow.sh
+++ scripts/data_manipulation/standard_rnaseq_workflow.sh
@@ -1,8 +1,18 @@
 #!/usr/bin/env bash
 
-#This scripts runs the standard RNA-seq preprocessing workflow on all fastq files within a directory
 
+#This scripts runs the standard RNA-seq preprocessing workflow on all fastq files within a directory
 # STAR -> samtools -> HTSeq
+
+set -u
+set -o errexit
+#set -o pipefail
+set -o nounset
+set -o xtrace
+set -o verbose
+
+#STEP ZERO: Ensure file system is set-up correctly
+
 
 #STEP ONE: Perform STAR alignment on all read pairs
 
