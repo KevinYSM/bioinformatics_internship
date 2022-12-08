08c517ba28d7435bc9f4ceee7539348a0f00f7e0
KevinYSM <22753615@student.uwa.edu.au>
2022-12-08 01:31:33 +0000

    fixed HTSEQ script

--- scripts/data_manipulation/standard_rnaseq_scripts/HTSEQ_count.sh
+++ scripts/data_manipulation/standard_rnaseq_scripts/HTSEQ_count.sh
@@ -12,5 +12,5 @@ output_dir="$2"
 #gtf=genome/Homo_sapiens.GRCh37.75.sorted.gtf
 gtf=/data/local/reference/igenomes/Homo_sapiens/NCBI/GRCh38/Annotation/Genes/genes.gtf
 
-htseq-count -r name -q -f bam -s reverse -m intersection-strict -o $output_dir/$(basename "$bam" ".bam").s_reverse.gene_counts  
+htseq-count -r name -q -f bam -s reverse -m intersection-strict $bam "$gtf" > $output_dir/$(basename "$bam" ".bam").s_reverse.gene_counts 
 wait
