0b65061efe764c2abc20315981258b763ea7222d
KevinYSM <22753615@student.uwa.edu.au>
2022-12-15 08:44:17 +0000

    attempted fixes to complete rnaseq workflow

--- scripts/nextflow/complete_rnaseq_workflow_2/bin/SAM_sort.sh
+++ scripts/nextflow/complete_rnaseq_workflow_2/bin/SAM_sort.sh
@@ -4,9 +4,9 @@ set -u
 set -o errexit
 set -o pipefail
 set -o nounset
-set -o xtrace
+#set -o xtrace
 set -o verbose
 
 bam=$1
 
-samtools index  $1
+samtools sort -O BAM -o $(basename "$bam" ".bam").sorted.bam "$bam"
